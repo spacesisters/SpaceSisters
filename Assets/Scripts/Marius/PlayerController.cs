48a869a387346eefd22a1c3a1b05e2ff8e2bdb16
Marius <schneeekind@gmail.com>
2019-05-25 16:23:03 +0200

    Updated my Scene with animated gravity push and pull mechanic and SFX. Jump with space. Shoot with c. Gravity push and pull with y and x.

--- Assets/Scripts/Marius/PlayerController.cs
+++ Assets/Scripts/Marius/PlayerController.cs
@@ -8,21 +8,46 @@ public class PlayerController : MonoBehaviour
     public float maxSpeed;
     public float xAcc;
     public GameObject playerCapsule;
+    public GameObject forceSphere;
     public GameObject shot;
 
+    public float maxMagnetRadius;
+    public float maxMagnetForce;
+
+    public AudioClip sfxJump;
+    public AudioClip sfxImpact;
+    public AudioClip sfxShot;
+    public AudioClip sfxForcefield;
+
     private Rigidbody rb;
-    private bool isGrounded;
     private Animator anim;
+    private Material mat;
+
     private int shotDir;
+    private float magnetRadius;
+
+    private bool isGrounded;
+    private bool isPulling;
+    private bool isPushing;
+
+    private AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         isGrounded = true;
-        anim = GetComponent<Animator>();
+        anim = playerCapsule.GetComponent<Animator>();
+        magnetRadius = 0.0f;
+        forceSphere.transform.localScale = new Vector3(magnetRadius, magnetRadius, 1.5f);
+        mat = forceSphere.GetComponent<Renderer>().material;
+        audioSource = GetComponent<AudioSource>();
     }
 
+    public void playImpactSound()
+    {
+        audioSource.PlayOneShot(sfxImpact);
+    }
     
     private void OnCollisionEnter()
     {
@@ -42,13 +67,86 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.RightControl))
+        if (Input.GetKeyDown(KeyCode.C))
         {
+            audioSource.PlayOneShot(sfxShot);
             GameObject tmp_shot = (GameObject)Instantiate(shot, playerCapsule.transform.position, playerCapsule.transform.rotation);
             if (tmp_shot != null)
             {
                 tmp_shot.GetComponent<Shot>().SetDirection(shotDir);
+                tmp_shot.GetComponent<Shot>().SetPlayer(this);
                 //Debug.Log(shotDir);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            isPushing = true;
+            forceSphere.SetActive(true);
+            mat.SetVector("Color_45AE0E17", Color.red * 8f);
+        }
+        else if (Input.GetKey(KeyCode.X))
+        {
+            isPulling = true;
+            forceSphere.SetActive(true);
+            mat.SetVector("Color_45AE0E17", Color.blue * 8f);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Y))
+        {
+            isPushing = false;
+        }
+
+        if (Input.GetKeyUp(KeyCode.X))
+        {
+            isPulling = false;
+        }
+
+        if (isPushing || isPulling)
+        {
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+            if (magnetRadius < 7)
+            {
+                magnetRadius += 0.8f;
+                forceSphere.transform.localScale = new Vector3(magnetRadius, magnetRadius, 1.5f);
+            }
+
+            Collider[] colliders = Physics.OverlapSphere(transform.position, maxMagnetRadius);
+            foreach (Collider col in colliders)
+            {
+                if (col.gameObject.GetComponent<Rigidbody>() != null
+                    && col.gameObject.layer != 12) // check if gameobject is not a shot
+                {
+                    Rigidbody rb = col.gameObject.GetComponent<Rigidbody>();
+                    Vector3 forceDirection = rb.position - transform.position;
+                    float distance = forceDirection.magnitude;
+                    if (isPushing)
+                    {
+                        rb.AddForce(forceDirection.normalized * maxMagnetForce * (maxMagnetRadius - distance));
+                        
+                    }
+                    else if (isPulling)
+                    {
+                        rb.AddForce(2 * -forceDirection.normalized * maxMagnetForce * (maxMagnetRadius - distance));
+                    }
+                    //Debug.Log("Radius: " + maxMagnetRadius + " - Distance: " + distance + " - Force: " + maxMagnetForce);
+                    //Debug.Log("Force Direction: " + forceDirection);
+                }
+            }
+        }
+        else
+        {
+            if (magnetRadius > 0)
+            {
+                magnetRadius -= 0.4f;
+                forceSphere.transform.localScale = new Vector3(magnetRadius, magnetRadius, 1.5f);
+                if (magnetRadius < 0.5)
+                {
+                    forceSphere.SetActive(false);
+                }
             }
         }
 
@@ -81,6 +179,7 @@ public class PlayerController : MonoBehaviour
         if (isGrounded && Input.GetKeyDown(KeyCode.Space))
         {
             //Debug.Log("Jump!");
+            audioSource.PlayOneShot(sfxJump);
             rb.AddForce(new Vector3(0, jumpForce * 100.0f, 0));
         }
 
