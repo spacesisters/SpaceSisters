a2e1a89798aeca9756108a1254b088b43724b04b
Wukong <44801751+SetoFckingKaiba@users.noreply.github.com>
2019-06-04 12:49:36 +0200

    Auto aiming bullets

--- Assets/Scripts/Artur/ArturBulletScript.cs
+++ Assets/Scripts/Artur/ArturBulletScript.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class ArturBulletScript : MonoBehaviour
 {
     public float bulletVelocity;
+    public float targetRadius;
+    public float targetVelocity;
     public LayerMask damagable;
+    public LayerMask autoAim;
     
     private Rigidbody rBody;
     private SphereCollider sphereCollider;
@@ -19,6 +22,28 @@ public class ArturBulletScript : MonoBehaviour
 
     private void Update()
     {
+        Collider[] targets = Physics.OverlapSphere(transform.position, targetRadius, autoAim);
+        Vector3 direction = Vector3.zero;
+        float distance = float.MaxValue;
+        Collider target = null;
+        foreach (Collider c in targets)
+        {
+            direction = transform.position - c.transform.position;
+            if (direction.magnitude < distance)
+            {
+                distance = direction.magnitude;
+                target = c;
+                direction = transform.position - c.transform.position;
+            }
+        }
+
+        if (target != null)
+        {
+            //rBody.AddForce(direction.normalized * targetVelocity, ForceMode.VelocityChange);
+            rBody.velocity = - direction.normalized * targetVelocity;
+        }
+
+
         Collider[] colliders = Physics.OverlapSphere(transform.position, sphereCollider.radius * 0.525f, damagable);
         // TODO: Apply damage.
     }
@@ -28,5 +53,8 @@ public class ArturBulletScript : MonoBehaviour
         sphereCollider = GetComponent<SphereCollider>();
         Gizmos.color = Color.white;
         Gizmos.DrawWireSphere(transform.position, sphereCollider.radius * 0.525f);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, 3);
+
     }
 }
