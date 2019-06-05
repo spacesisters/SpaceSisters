99112c8e521f7da70d545841ee699a00f9ec785a
Wukong <44801751+SetoFckingKaiba@users.noreply.github.com>
2019-06-05 13:42:19 +0200

    Added second player

---
+++ Assets/Scripts/Artur/ArturPlayerTwoController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArturPlayerTwoController : ArturBasePlayerController
+{
+    
+    private ArturForcefieldController forcefieldController;
+    
+
+    private void Start()
+    {
+        forcefieldController = transform.GetChild(0).GetComponent<ArturForcefieldController>();
+        forcefieldController.Initialize(-1);
+        base.Initialize();
+    }
+
+    private new void Update()
+    {
+        base.Update();
+        TranslateInput();
+        if (doForcefield)
+        {
+            forcefieldController.ActivateForcefield();
+            energy -= energyDrainPerSecond * Time.deltaTime;
+        }
+
+        
+    }
+
+    private new void FixedUpdate()
+    {
+        base.FixedUpdate();
+        
+    }
+
+    private new void TranslateInput()
+    {
+        base.TranslateInput();
+        
+    }
+
+}
