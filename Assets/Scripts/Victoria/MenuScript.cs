b9bb96fe6939225d44db97d8acfaab5cff78af66
Victoria Wagner <vs.wagner@yahoo.de>
2019-06-18 10:15:54 +0200

    remved bugs from menu

---
+++ Assets/Scripts/Victoria/MenuScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public void ChangeMenuScene(string scenename)
+    {
+        SceneManager.LoadScene(scenename);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Player quit game!");
+    }
+}
