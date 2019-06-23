da3d81165d5ea86638fd011aa4925a42e7738c7c
Michael Blesel <michaelblesel@web.de>
2019-06-23 14:59:56 +0200

    Some optimisations on the gravity reversal

--- Assets/Scripts/Michael/MichaelGravityReverseField.cs
+++ Assets/Scripts/Michael/MichaelGravityReverseField.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class MichaelGravityReverseField : MonoBehaviour
 {
-    Vector3 collision_enter_p1, collision_exit_p1;
-    Vector3 collision_enter_p2, collision_exit_p2;
+    Vector3 collision_enter_p1, collision_exit_p1, collision_enter_velocity_p1;
+    Vector3 collision_enter_p2, collision_exit_p2, collision_enter_velocity_p2;
 
     void Start()
     {
@@ -23,11 +23,13 @@ public class MichaelGravityReverseField : MonoBehaviour
         {
             ArturBasePlayerController player = collided_object.GetComponent<ArturBasePlayerController>();
             collision_enter_p1 = collided_object.transform.position - this.transform.position;
+            collision_enter_velocity_p1 = collided_object.GetComponent<Rigidbody>().velocity;
         }
         else if(collided_object.tag == "Player2")
         {
             ArturBasePlayerController player = collided_object.GetComponent<ArturBasePlayerController>();
             collision_enter_p2 = collided_object.transform.position - this.transform.position;
+            collision_enter_velocity_p2 = collided_object.GetComponent<Rigidbody>().velocity;
         }
     }
 
@@ -42,50 +44,30 @@ public class MichaelGravityReverseField : MonoBehaviour
 
             if (this.transform.rotation.eulerAngles.z == 0 || this.transform.rotation.eulerAngles.z == 180)
             {
-                if(collision_enter_p1.x < 0 && collision_exit_p1.x > 0)
+                if(collision_enter_velocity_p1.x > 0 && collision_enter_p1.x <= collision_exit_p1.x)
                 {
                     player.ReverseGravity();
-                    collision_enter_p1 = new Vector3(0,0,0);
-                    collision_exit_p1 = new Vector3(0,0,0);
                 }
-                else if(collision_enter_p1.x > 0 && collision_exit_p1.x < 0)
+                else if(collision_enter_velocity_p1.x < 0 && collision_enter_p1.x >= collision_exit_p1.x)
                 {
                     player.ReverseGravity();
-                    collision_enter_p1 = new Vector3(0,0,0);
-                    collision_exit_p1 = new Vector3(0,0,0);
-                }
-                // Character sometimes goes to fast. In that case it looks like enter and exit
-                // coordinates are the same.
-                else
-                {
-                    player.ReverseGravity();
-                    collision_enter_p1 = new Vector3(0,0,0);
-                    collision_exit_p1 = new Vector3(0,0,0);
                 }
             }
             else if (this.transform.rotation.eulerAngles.z == 90 || this.transform.rotation.eulerAngles.z == -90)
             {
-                if(collision_enter_p1.y < 0 && collision_exit_p1.y > 0)
+                if(collision_enter_velocity_p1.y > 0 && collision_enter_p1.y <= collision_exit_p1.y)
                 {
                     player.ReverseGravity();
-                    collision_enter_p1 = new Vector3(0,0,0);
-                    collision_exit_p1 = new Vector3(0,0,0);
                 }
-                else if(collision_enter_p1.y > 0 && collision_exit_p1.y < 0)
+                else if(collision_enter_velocity_p1.y < 0 && collision_enter_p1.y >= collision_exit_p1.y)
                 {
                     player.ReverseGravity();
-                    collision_enter_p1 = new Vector3(0,0,0);
-                    collision_exit_p1 = new Vector3(0,0,0);
-                }
-                // Character sometimes goes to fast. In that case it looks like enter and exit
-                // coordinates are the same.
-                else
-                {
-                    player.ReverseGravity();
-                    collision_enter_p1 = new Vector3(0,0,0);
-                    collision_exit_p1 = new Vector3(0,0,0);
                 }
             }
+
+            collision_enter_p1 = new Vector3(0,0,0);
+            collision_exit_p1 = new Vector3(0,0,0);
+            collision_enter_velocity_p1 = new Vector3(0,0,0);
         }
         else if(collided_object.tag == "Player2")
         {
@@ -94,50 +76,29 @@ public class MichaelGravityReverseField : MonoBehaviour
 
             if (this.transform.rotation.eulerAngles.z == 0 || this.transform.rotation.eulerAngles.z == 180)
             {
-                if(collision_enter_p2.x < 0 && collision_exit_p2.x > 0)
+                if(collision_enter_velocity_p2.x > 0 && collision_enter_p2.x <= collision_exit_p2.x)
                 {
                     player.ReverseGravity();
-                    collision_enter_p2 = new Vector3(0,0,0);
-                    collision_exit_p2 = new Vector3(0,0,0);
                 }
-                else if(collision_enter_p2.x > 0 && collision_exit_p2.x < 0)
+                else if(collision_enter_velocity_p2.x < 0 && collision_enter_p2.x >= collision_exit_p2.x)
                 {
                     player.ReverseGravity();
-                    collision_enter_p2 = new Vector3(0,0,0);
-                    collision_exit_p2 = new Vector3(0,0,0);
-                }
-                // Character sometimes goes to fast. In that case it looks like enter and exit
-                // coordinates are the same.
-                else
-                {
-                    player.ReverseGravity();
-                    collision_enter_p1 = new Vector3(0,0,0);
-                    collision_exit_p1 = new Vector3(0,0,0);
                 }
             }
             else if (this.transform.rotation.eulerAngles.z == 90 || this.transform.rotation.eulerAngles.z == -90)
             {
-                if(collision_enter_p2.y < 0 && collision_exit_p2.y > 0)
+                if(collision_enter_velocity_p2.y > 0 && collision_enter_p2.y <= collision_exit_p2.y)
                 {
                     player.ReverseGravity();
-                    collision_enter_p2 = new Vector3(0,0,0);
-                    collision_exit_p2 = new Vector3(0,0,0);
                 }
-                else if(collision_enter_p2.y > 0 && collision_exit_p2.y < 0)
+                else if(collision_enter_velocity_p2.y < 0 && collision_enter_p2.y >= collision_exit_p2.y)
                 {
                     player.ReverseGravity();
-                    collision_enter_p2 = new Vector3(0,0,0);
-                    collision_exit_p2 = new Vector3(0,0,0);
-                }
-                // Character sometimes goes to fast. In that case it looks like enter and exit
-                // coordinates are the same.
-                else
-                {
-                    player.ReverseGravity();
-                    collision_enter_p1 = new Vector3(0,0,0);
-                    collision_exit_p1 = new Vector3(0,0,0);
                 }
             }
+            collision_enter_p2 = new Vector3(0,0,0);
+            collision_exit_p2 = new Vector3(0,0,0);
+            collision_enter_velocity_p2 = new Vector3(0,0,0);
         }
     }
 
