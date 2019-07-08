2e7f2d1d999c5977fc5ab689683ec2c61a9ca0b5
Marius <schneeekind@gmail.com>
2019-07-08 11:39:45 +0200

    Created new HDRP-Scene for better lighting

---
+++ Assets/Scenes/Marius/HDRP_Scene/Scripts/graphic_background_script.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class graphic_background_script : MonoBehaviour
+{
+    public Camera camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 new_position = transform.position;
+        Vector3 camera_position = camera.transform.position;
+        new_position.x = camera_position.x;
+        new_position.y = camera_position.y;
+        transform.position = new_position;
+    }
+}
---
+++ Assets/Scenes/Marius/HDRP_Scene/Scripts/graphic_camera_script.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class graphic_camera_script : MonoBehaviour
+{
+    float speed = 0.25f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 new_position = transform.position;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            new_position.x -= speed;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            new_position.x += speed;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            new_position.y += speed;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            new_position.y -= speed;
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            new_position.z += speed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            new_position.z -= speed;
+        }
+
+        transform.position = new_position;
+    }
+}
