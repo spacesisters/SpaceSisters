78e17e2bdd637428ee3614740e6b4c213b82ddf0
Marius <schneeekind@gmail.com>
2019-07-25 15:49:19 +0200

    Collectable Materials

--- Assets/Scripts/Marius/wireframe_script.cs
+++ Assets/Scripts/Marius/wireframe_script.cs
@@ -10,6 +10,7 @@ public class wireframe_script : MonoBehaviour
     private float time_offset;
     public float yChange = 0.01f;
     public float yOffset = -3.0f;
+    private float epsilon = 0.2f;
 
     // Start is called before the first frame update
     void Start()
@@ -25,11 +26,11 @@ public class wireframe_script : MonoBehaviour
         wireframeMat.SetVector("_offset", new Vector2(cameraPos.x * 0.5f + time_offset, 0.0f));
 
         new_position.x = cameraPos.x;
-        if ((cameraPos.y + yOffset) > new_position.y)
+        if ((cameraPos.y + yOffset) > (new_position.y + epsilon))
         {
             new_position.y += yChange;
         }
-        else if ((cameraPos.y + yOffset) < new_position.y)
+        else if ((cameraPos.y + yOffset) < (new_position.y - epsilon))
         {
             new_position.y -= yChange;
         }
