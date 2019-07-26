1829ae6069997f89580bbc7b10606f0032bc8acf
Marius Pierenkemper <5pierenk@informatik.uni-hamburg.de>
2019-07-27 00:01:45 +0200

    gfx

---
+++ Assets/Scripts/Marius/background_slider_ingame.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class background_slider_ingame : MonoBehaviour
+{
+    public Vector2 offset;
+    public Material mat;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        offset.x += 0.0002f;
+        offset.y = Mathf.Sin(Time.time) * 0.01f;
+        mat.SetVector("_offset", offset);
+        //mat.mainTextureOffset = offset;
+        //print(mat.mainTextureOffset);
+        //print(mat.GetTextureOffset("_MainTex"));
+    }
+}
