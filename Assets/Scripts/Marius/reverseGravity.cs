495ecfc493e045b8675aad5e9d0b5973b99bfac9
Marius Pierenkemper <5pierenk@informatik.uni-hamburg.de>
2019-07-27 08:52:51 +0200

    Prefab for: Movable Obj, Ground Ice/Fire

--- Assets/Scripts/Marius/reverseGravity.cs
+++ Assets/Scripts/Marius/reverseGravity.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class reverseGravity : MonoBehaviour
 {
     private Rigidbody rb;
-    public float gForce = 9.81f;
+    private float gForce = 9.81f;
+    public bool isReversed = false;
 
     // Start is called before the first frame update
     void Start()
@@ -16,11 +17,16 @@ public class reverseGravity : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (isReversed)
+        {
+            gForce *= -1;
+        }
+
         rb.AddForce(new Vector3(0.0f, gForce, 0.0f), ForceMode.Acceleration);
     }
 
     public void reverseG()
     {
-        gForce *= -1.0f;
+        isReversed = !isReversed;
     }
 }
