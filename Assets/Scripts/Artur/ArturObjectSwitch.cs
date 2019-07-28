e3c8fef8204017c7dbad9a2c74ebe4737044e6c4
Wukong <44801751+SetoFckingKaiba@users.noreply.github.com>
2019-07-28 14:43:23 +0200

    added portal and generic switch

---
+++ Assets/Scripts/Artur/ArturObjectSwitch.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArturObjectSwitch : MonoBehaviour
+{
+
+    public GameObject target;
+    public bool permanent;
+
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player1") || other.CompareTag("Player2"))
+        {
+            target.SetActive(!target.activeSelf);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player1") || other.CompareTag("Player2"))
+        {
+            if (!permanent)
+            {
+                target.SetActive(!target.activeSelf);
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/Artur/ArturPortalScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArturPortalScript : MonoBehaviour
+{
+    public Transform otherPortalSpawnLocation;
+    public bool canTraverse;
+    public bool reverseGravityOnEnter;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player1") || other.gameObject.CompareTag("Player2"))
+        {
+            if (canTraverse)
+            {
+                other.gameObject.transform.position = otherPortalSpawnLocation.position;
+                if (reverseGravityOnEnter)
+                {
+                    other.GetComponent<ArturBasePlayerController>().ReverseGravity();
+                }
+            }
+        }
+    }
+
+}
